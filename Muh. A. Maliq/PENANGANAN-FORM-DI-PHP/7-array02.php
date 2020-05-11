0cf52ee27959cc353991ef1ac598760cd2fd2af6
contract-impossible <contrack.impossible@protonmail.com>
2020-05-11 22:52:31 +0700

    Menambahkan Modul 7-array02

---
+++ Muh. A. Maliq/PENANGANAN-FORM-DI-PHP/7-array02.php
@@ -0,0 +1,17 @@
+<?php
+	$arrNilai = array(
+		'Ani'	=> 80,
+		"Otim"	=> 90,
+		"Ana"	=> 75,
+		"Budi"	=> 85
+	);
+	echo $arrNilai["Ani"];
+	echo $arrNilai["Otim"];
+
+	$arrNilai = array();
+	$arrNilai["Ami"]	= 80;
+	$arrNilai["Asma"]	= 95;
+	$arrNilai["Sri"]	= 77;
+	echo $arrNilai["Asma"];
+	echo $arrNilai["Ami"];
+?>
