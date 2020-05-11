8d816fe2e637b885ee77468e3f78642c248dfe92
contract-impossible <contrack.impossible@protonmail.com>
2020-05-11 22:53:00 +0700

    Menambahkan Modul 7-array06

---
+++ Muh. A. Maliq/PENANGANAN-FORM-DI-PHP/7-array06.php
@@ -0,0 +1,12 @@
+<?php
+	$presiden = array(
+		01		=> "Habibie",
+		1		=> "Gusdur",
+		"1"		=> "Megawati",
+		1.5		=> "SBY",
+		true 	=> "Jokowi"
+	);
+	echo "<pre>";
+	print_r($presiden);
+	echo "</pre>";
+?>
