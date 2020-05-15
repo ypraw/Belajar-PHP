264b09e4f29dc46b8bbfa56ae745134b2a6bc3c4
glxy08 <22082002+glxy08@users.noreply.github.com>
2020-05-15 12:41:33 +0700

    hapus

--- Nisa A.M/penanganan-form/proses03.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(isset($_GET['input'])){
-    $nama = $_GET['name'];
-    $email = $_GET['email'];
-    echo "Nama anda : <b>$nama</b><br/><br/>";
-    echo "E-mail anda : <b>$email</b>";
-}
